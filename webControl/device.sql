ac911c3fe350b480d465005a111cee5ce91ec9bc
Cha0s <457318796@qq.com>
2018-02-17 00:19:45 +0800

    update databae
    update databae

--- webControl/device.sql
+++ webControl/device.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2018 年 02 月 09 日 13:16
+-- 生成日期: 2018 年 02 月 17 日 00:18
 -- 服务器版本: 5.5.53
 -- PHP 版本: 5.4.45
 
@@ -29,17 +29,13 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `device` (
   `num` int(11) NOT NULL AUTO_INCREMENT,
   `ID` int(11) NOT NULL,
-  `data_pm` int(11) NOT NULL,
-  `data_co` int(11) NOT NULL,
-  `data_pwm` int(11) NOT NULL,
-  `data_temp` int(11) NOT NULL,
-  `data_switch_one` int(11) NOT NULL,
-  `data_switch_two` int(11) NOT NULL,
-  `data_switch_three` int(11) NOT NULL,
-  `data_switch_four` int(11) NOT NULL,
-  `data_switch_mode` int(11) NOT NULL,
+  `data_airQuality` float NOT NULL,
+  `data_distance` int(11) NOT NULL,
+  `data_voice` int(11) NOT NULL,
+  `data_light` int(11) NOT NULL,
+  `data_humi` float NOT NULL,
+  `data_temp` float NOT NULL,
   `client_id` varchar(100) NOT NULL,
-  `name` text NOT NULL,
   `timestamp` varchar(30) NOT NULL,
   `online_or_not` int(11) NOT NULL,
   `user` char(100) NOT NULL,
@@ -50,14 +46,14 @@ CREATE TABLE IF NOT EXISTS `device` (
 -- 转存表中的数据 `device`
 --
 
-INSERT INTO `device` (`num`, `ID`, `data_pm`, `data_co`, `data_pwm`, `data_temp`, `data_switch_one`, `data_switch_two`, `data_switch_three`, `data_switch_four`, `data_switch_mode`, `client_id`, `name`, `timestamp`, `online_or_not`, `user`) VALUES
-(5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '7f0000010b5400000076', '121121', '2018 02 09 01:16:58pm', 0, '3'),
-(11, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, '7f0000010b5400000002', '', '2017 11 23 04:29:24am', 0, 'user'),
-(6, 2147483647, 212, 12, 121, 212, 12, 212, 2, 2, 1, '121212121', '教室1', '', 0, 'user'),
-(7, 2332231, 12, 12, 21, 12, 21, 12, 2, 1, 1, '12345678765432', '教室2', '', 1, 'user'),
-(8, 34334, 234, 234, 234, 234, 23, 23, 123, 23, 234, '1234554321', '教室3', '', 0, 'user'),
-(9, 214748372, 1, 3, 2, 3, 4, 5, 2, 23, 1, '6789876543211', '教室里1234', '', 1, 'user'),
-(10, 23322345, 1, 3, 2, 3, 4, 5, 2, 23, 1, '6789876543211', 'a教学楼', '', 1, 'user');
+INSERT INTO `device` (`num`, `ID`, `data_airQuality`, `data_distance`, `data_voice`, `data_light`, `data_humi`, `data_temp`, `client_id`, `timestamp`, `online_or_not`, `user`) VALUES
+(5, 0, 0, 0, 0, 0, 0, 0, '7f0000010b5400000076', '2018 02 09 01:19:11pm', 0, '3'),
+(11, 1, 1, 1, 1, 1, 0, 0, '7f0000010b5400000002', '2017 11 23 04:29:24am', 0, 'user'),
+(6, 2147483647, 212, 12, 121, 212, 12, 212, '121212121', '', 0, 'user'),
+(7, 2332231, 12, 12, 21, 12, 21, 12, '12345678765432', '', 1, 'user'),
+(8, 34334, 234, 234, 234, 234, 23, 23, '1234554321', '', 0, 'user'),
+(9, 214748372, 1, 3, 2, 3, 4, 5, '6789876543211', '', 1, 'user'),
+(10, 23322345, 1, 3, 2, 3, 4, 5, '6789876543211', '', 1, 'user');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
